99572da7ba644d4f89219e8837b3ada3e804ff94
nishan-pradhan06 <nishanpradhan500@gmail.com>
2026-06-10 10:43:32 +0545

    analytics

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 val keystoreProperties = Properties()
@@ -59,4 +60,19 @@ android {
 
 flutter {
     source = "../.."
+}
+
+
+dependencies {
+  // Import the Firebase BoM
+  implementation(platform("com.google.firebase:firebase-bom:34.14.0"))
+
+
+  // TODO: Add the dependencies for Firebase products you want to use
+  // When using the BoM, don't specify versions in Firebase dependencies
+  implementation("com.google.firebase:firebase-analytics")
+
+
+  // Add the dependencies for any other desired Firebase products
+  // https://firebase.google.com/docs/android/setup#available-libraries
 }
